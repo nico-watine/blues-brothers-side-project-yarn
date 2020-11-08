02a96a1269562901e9808011c8246284b4f57038
Nico Watine <nico@studioncreations.com>
2020-11-08 17:31:23 -0600

    Copy content

---
+++ src/pages/new.php
@@ -0,0 +1,16 @@
+---
+url: /new
+title:
+description:
+og-title:
+og-description:
+og-image:
+body-id: index
+body-class: index
+---
+<section class="soul-food-cafe-section">
+	<div class="row" style="height: 560px;">
+
+	</div>
+</section>
+
